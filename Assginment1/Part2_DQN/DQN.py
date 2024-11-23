b096c418cd6b44d09f52631eb54e4f69cb570763
ozelbaz <oz.elbaz@gmail.com>
2024-11-23 23:14:21 +0200

    Added the architecture of the net

--- Assginment1/Part2_DQN/DQN.py
+++ Assginment1/Part2_DQN/DQN.py
@@ -1,7 +1,29 @@
 import gymnasium as gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from collections import deque
+import random
 
 env = gym.make("CartPole-v1", render_mode='human')
 env.reset()
 env.render()
 state_dim = env.observation_space.shape[0]  # 4-dimensional state
 action_dim = env.action_space.n  # 2 possible actions
+
+class QNetwork(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 128)
+        self.fc4 = nn.Linear(128, 128)
+        self.fc5 = nn.Linear(128, action_dim)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        x = torch.relu(self.fc4(x))
+        return self.fc5(x)  # Output Q-values for all actions
